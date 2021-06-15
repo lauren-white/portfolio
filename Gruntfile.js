b42a15a83a69d0bab04b5c646be6e6d0c674084a
lauren-white <51828361+lauren-white@users.noreply.github.com>
2021-06-15 13:45:19 -0500

    Project start

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+
+module.exports = function(grunt) {
+    const sass = require('node-sass');
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      sass: {
+        options: {
+            implementation: sass,
+            sourceMap: true
+        },
+        dist: {
+            files: {
+                'css/main.css': 'css/global.scss'
+            }
+        }
+    },
+      watch: {
+        css: {
+          files: '**/*.scss',
+          tasks: ['sass']
+        }
+      }
+    });
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default',['watch']);
+  }
