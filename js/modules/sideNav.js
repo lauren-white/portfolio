8a5e90eb55c75879f355f82d17451a8b1418471e
lauren-white <51828361+lauren-white@users.noreply.github.com>
2021-07-19 10:23:17 -0500

    Hamburger nav animation

--- js/modules/sideNav.js
+++ js/modules/sideNav.js
@@ -3,10 +3,15 @@ export default class SideNav {
     this.addListener();
   }
   addListener() {
-    let elem = document.querySelector(".js-sticky-nav");
+    let elem = document.querySelector(".js-side-nav");
 
     elem.addEventListener("click", (ev) => {
       ev.preventDefault();
+      if (elem.classList.contains("is-active")) {
+        elem.classList.remove("is-active");
+      } else {
+        elem.classList.add("is-active");
+      }
     });
   }
 }
