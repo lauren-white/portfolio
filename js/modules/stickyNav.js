5556fb23175b9514ca2591c35c5c4c3ca904f62a
lauren-white <51828361+lauren-white@users.noreply.github.com>
2021-11-29 13:29:53 -0600

    Improve sticky nav

--- js/modules/stickyNav.js
+++ js/modules/stickyNav.js
@@ -1,47 +1,56 @@
 export default class StickyNav {
   constructor() {
-    this.scrolling = false;
+    this.didScroll = false;
     this.lastScrollPosition = window.scrollY;
     this.timerRunning = false;
-    this.addListener();
+    this.hamburgerNavShowing = false;
+    this.screenSizeCheck();
+    this.addResizeListener();
+    this.addScrollListener();
     this.throttle();
   }
+  screenSizeCheck() {
+    //770px is when we switch to hamburger nav
+    if (window.innerWidth <= 770) {
+      this.hamburgerNavShowing = true;
+    } else {
+      this.hamburgerNavShowing = false;
+    }
+  }
+  addResizeListener() {
+    window.addEventListener("resize", (ev) => {
+      this.screenSizeCheck();
+    });
+  }
 
-  addListener() {
+  addScrollListener() {
     document.addEventListener("scroll", (ev) => {
-      this.scrolling = true;
+      this.didScroll = true;
     });
   }
   throttle() {
     setInterval(() => {
-      if (this.scrolling) {
-        this.scolling = false;
+      if (this.didScroll && !this.hamburgerNavShowing) {
         if (window.scrollY < this.lastScrollPosition) {
           this.addSticky();
         } else {
           this.removeSticky();
         }
-
+        this.didScroll = false;
         this.lastScrollPosition = window.scrollY;
       }
-    }, 300);
+    }, 250);
   }
   addSticky() {
-    let elem = document.querySelector(".header__nav");
+    let elem = document.querySelector(".header__nav").parentElement;
     if (!elem.classList.contains("header__sticky")) {
       elem.classList.add("header__sticky");
     }
   }
   removeSticky() {
-    if (!this.timerRunning) {
-      setTimeout(() => {
-        let elem = document.querySelector(".header__nav");
-        if (elem.classList.contains("header__sticky")) {
-          elem.classList.remove("header__sticky");
-        }
-        this.timerRunning = true
-
-      }, 5000);
+    let elem = document.querySelector(".header__nav").parentElement;
+    if (elem.classList.contains("header__sticky")) {
+      elem.classList.remove("header__sticky");
     }
   }
 }
